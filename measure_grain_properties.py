284cedb989dc84734a51bd73bdb65aa16f7ae917
Max Frei <max.frei@uni-due.de>
2022-03-07 12:52:39 +0100

    Measure mask properties.

--- measure_grain_properties.py
+++ measure_grain_properties.py
@@ -5,7 +5,18 @@ import fire
 from custom_postprocessing import SimplifyGrainBoundaries
 from paddle.custom_types import AnyPath
 from paddle.data import MaskRCNNDataset
-from paddle.postprocessing import PickleAnnotation, Postprocessor
+from paddle.postprocessing import (
+    MeasureMaskProperties,
+    PickleAnnotation,
+    Postprocessor,
+    calculate_areas,
+    calculate_distances_to_image_border,
+    calculate_major_axis_lengths,
+    calculate_maximum_feret_diameters,
+    calculate_minimum_feret_diameters,
+    calculate_minor_axis_lengths,
+    calculate_perimeters,
+)
 
 
 def measure_grain_properties(data_root: AnyPath, subset: str):
@@ -18,9 +29,29 @@ def measure_grain_properties(data_root: AnyPath, subset: str):
 
     subset_folder_path = data_root / subset
 
+    measurement_fcns = {
+        "mask_feret_diameter_max": calculate_maximum_feret_diameters,
+        "mask_feret_diameter_min": calculate_minimum_feret_diameters,
+        "mask_area": calculate_areas,
+        "mask_perimeter": calculate_perimeters,
+        "mask_axis_length_min": calculate_minor_axis_lengths,
+        "mask_axis_length_max": calculate_major_axis_lengths,
+        "mask_distance_to_image_border": calculate_distances_to_image_border,
+    }
+
     post_processing_steps = [
+        MeasureMaskProperties(measurement_fcns=measurement_fcns),
         SimplifyGrainBoundaries(),
-        PickleAnnotation(subset_folder_path, exclude_keys=["masks"]),
+        PickleAnnotation(
+            subset_folder_path,
+            exclude_keys=[
+                "masks",
+                "area",
+                "iscrowd",
+                "slice_index_x",
+                "slice_index_y",
+            ],
+        ),
     ]
 
     postprocessor = Postprocessor(

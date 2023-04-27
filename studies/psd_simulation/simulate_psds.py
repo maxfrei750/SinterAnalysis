5f4d18675d4a232316559a3491898519dd8cc504
Max Frei <max.frei@uni-due.de>
2023-04-27 14:23:30 +0200

    Study psd_simulation: Added docstrings and missing type hints.

--- studies/psd_simulation/simulate_psds.py
+++ studies/psd_simulation/simulate_psds.py
@@ -1,3 +1,27 @@
+"""This module provides classes and functions to simulate and plot particle size
+distributions. The module defines two data classes: TrueParticleSizeDistribution and
+MeasuredParticleSizeDistribution. The former defines a true particle size distribution
+based on its geometric mean diameter and geometric standard deviation, and the latter
+simulates a measured particle size distribution based on the true distribution and a
+specified measurement standard deviation. The module also provides a function,
+plot_psd_comparison, which generates a plot comparing a true particle size distribution
+and multiple simulated measured particle size distributions. The function plots each
+distribution with its corresponding measurement parameters and error values.
+
+Classes:
+    TrueParticleSizeDistribution: A class representing a true particle size
+        distribution.
+    MeasuredParticleSizeDistribution: A class representing a simulated measured particle
+        size distribution.
+
+Functions:
+    plot_psd_comparison: A function that generates a plot comparing a true particle size
+        distribution and multiple simulated measured particle size distributions.
+
+Constants:
+    SCRIPT_DIR: A pathlib.Path object representing the directory of the current script.
+"""
+
 import dataclasses
 from pathlib import Path
 from typing import List
@@ -12,11 +36,26 @@ SCRIPT_DIR = Path(__file__).parent
 
 @dataclasses.dataclass
 class MeasuredParticleSizeDistribution:
+    """
+    Measured particle size distribution.
+
+    Attributes:
+        measurements_true (List[float]): List of true measurements.
+        measurement_standard_deviation (float): Measurement standard deviation.
+        random_seed (int): Random seed for measurement.
+    """
+
     measurements_true: List[float]
     measurement_standard_deviation: float
     random_seed: int = 0
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
+        """
+        Post-init method.
+
+        Initializes class attributes and calculates geometric mean diameter and
+        geometric standard deviation.
+        """
         self.random_number_generator = np.random.RandomState(self.random_seed)
         self.measurements_biased = [
             self.random_number_generator.normal(
@@ -37,24 +76,42 @@ class MeasuredParticleSizeDistribution:
             self.measurements_biased
         )
 
-    def plot(self, **kwargs):
-        """Plot the measured particle size distribution."""
+    def plot(self, **kwargs) -> None:
+        """
+        Plot the measured particle size distribution.
+        """
         sns.kdeplot(self.measurements_biased, clip=(0, float("inf")), **kwargs)
 
 
 @dataclasses.dataclass
 class TrueParticleSizeDistribution:
+    """
+    True particle size distribution.
+
+    Attributes:
+        geometric_standard_deviation (float): Geometric standard deviation.
+        geometric_mean_diameter (float): Geometric mean diameter.
+    """
+
     geometric_standard_deviation: float
     geometric_mean_diameter: float
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
+        """
+        Post-init method.
+
+        Initializes the log-normal distribution with given geometric standard deviation
+        and geometric mean diameter.
+        """
         self.distribution = scipy_stats.lognorm(
             s=np.log(self.geometric_standard_deviation),
             scale=self.geometric_mean_diameter,
         )
 
-    def plot(self, **kwargs):
-        """Plot the accurate particle size distribution."""
+    def plot(self, **kwargs) -> None:
+        """
+        Plot the accurate particle size distribution.
+        """
         x = np.logspace(-2, 2, num=1000)
         plt.plot(x, self.distribution.pdf(x), **kwargs)
 
@@ -64,7 +121,17 @@ class TrueParticleSizeDistribution:
         measurement_standard_deviation: float,
         random_seed: int = 0,
     ) -> MeasuredParticleSizeDistribution:
-        """Spawn a measurement of the particle size distribution."""
+        """
+        Spawn a measurement of the particle size distribution.
+
+        Args:
+            num_samples (int): Number of samples.
+            measurement_standard_deviation (float): Measurement standard deviation.
+            random_seed (int): Random seed.
+
+        Returns:
+            (MeasuredParticleSizeDistribution): Measured particle size distribution.
+        """
         return MeasuredParticleSizeDistribution(
             measurements_true=self.distribution.rvs(
                 size=num_samples, random_state=random_seed
@@ -75,12 +142,42 @@ class TrueParticleSizeDistribution:
 
 
 def plot_psd_comparison(
-    geometric_mean_diameter_true,
-    geometric_standard_deviation_true,
-    num_samples,
-    measurement_standard_deviations_percent,
+    geometric_mean_diameter_true: float,
+    geometric_standard_deviation_true: float,
+    num_samples: int,
+    measurement_standard_deviations_percent: List[float],
     random_seed=0,
-):
+) -> None:
+    """Plot the comparison between a true particle size distribution and a simulated
+    measured particle size distribution. The true distribution is defined by its
+    geometric mean diameter and geometric standard deviation. The function simulates the
+    measurement of the particle size distribution by generating a biased distribution
+    with a normal distribution and adding noise based on the specified measurement
+    standard deviation. The function generates multiple simulated measurements with
+    different measurement standard deviations, and for each measurement, it calculates
+    the error in the estimated geometric mean diameter and geometric standard deviation
+    relative to the true values. Finally, the function plots the true particle size
+    distribution and the simulated measurements, labeling each plot with the measurement
+    parameters and the calculated errors.
+
+    Args:
+        geometric_mean_diameter_true (float): The geometric mean diameter of the true
+            particle size distribution in pixels.
+        geometric_standard_deviation_true (float): The geometric standard deviation of
+            the true particle size distribution.
+        num_samples (int): The number of samples to generate for each simulated
+            measurement.
+        measurement_standard_deviations_percent (List[float]): A list of percentage
+            values representing the measurement standard deviation to use for each
+            simulated measurement, expressed as a percentage of the geometric mean
+            diameter of the true distribution.
+        random_seed (int, optional): The random seed to use for generating random
+            numbers. Default is 0.
+
+    Returns:
+        None
+    """
+
     psd_true = TrueParticleSizeDistribution(
         geometric_mean_diameter=geometric_mean_diameter_true,
         geometric_standard_deviation=geometric_standard_deviation_true,
